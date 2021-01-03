62ec63f62ce70b7e1862b2051d77f90ab1ce261f
Andre <andreoliveira.adv@hotmail.com>
2021-01-03 17:38:36 -0500

    Calculator Challenge - test JUnit

--- first-test/src/test/java/ca/andre/springboot/first_test/FirstTest.java
+++ first-test/src/test/java/ca/andre/springboot/first_test/FirstTest.java
@@ -4,22 +4,47 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class FirstTest {
-	
+
 	@Test
-	public void testStructure() {
-		
+	public void mustSumTwoNumbers() {
+
 		// scenario
+		Calculator calculator1 = new Calculator();
 		int number1 = 10, number2 = 5;
-		
+
 		// execution
-		int result = number1 + number2;
-		
+		int result = calculator1.sum(number1, number2);
+
 		// verification
-		// Assert.assertEquals(15, result);
-		// Assertions.assertThat(result).isBetween(14, 16);
-		// Assertions.assertThat(result).isEqualTo(15);
-		Assertions.assertThat(result).isGreaterThan(12);
+		Assertions.assertThat(result).isEqualTo(15);
 	}
-	
+
+	@Test(expected = RuntimeException.class)
+	public void shouldntSumNetagiveNumbers() {
+
+		// scenario
+		Calculator calculator2 = new Calculator();
+		int num1 = -10, num2 = 5;
+
+		// execution
+		int res = calculator2.sum(num1, num2);
+
+		// verification
+
+	}
+
+}
+
+class Calculator {
+
+	int sum(int num1, int num2) {
+
+		if (num1 < 0 || num2 < 0) {
+			throw new RuntimeException("The sum cannot contain negative numbers.");
+		} else {
+
+			return num1 + num2;
+		}
+	}
 
 }
