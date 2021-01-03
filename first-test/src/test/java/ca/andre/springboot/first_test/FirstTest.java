13817c9a4d0e9a99849f1580525ab75df174a1fd
Andre <andreoliveira.adv@hotmail.com>
2021-01-03 18:17:18 -0500

    Testing the challenge calculator

--- first-test/src/test/java/ca/andre/springboot/first_test/FirstTest.java
+++ first-test/src/test/java/ca/andre/springboot/first_test/FirstTest.java
@@ -9,11 +9,11 @@ public class FirstTest {
 	public void mustSumTwoNumbers() {
 
 		// scenario
-		Calculator calculator1 = new Calculator();
-		int number1 = 10, number2 = 5;
+		Calculator calculator = new Calculator();
+		int num1 = 10, num2 = 5;
 
 		// execution
-		int result = calculator1.sum(number1, number2);
+		int result = calculator.sum(num1, num2);
 
 		// verification
 		Assertions.assertThat(result).isEqualTo(15);
@@ -23,14 +23,67 @@ public class FirstTest {
 	public void shouldntSumNetagiveNumbers() {
 
 		// scenario
-		Calculator calculator2 = new Calculator();
+		Calculator calculator = new Calculator();
 		int num1 = -10, num2 = 5;
 
 		// execution
-		int res = calculator2.sum(num1, num2);
+		int result = calculator.sum(num1, num2);
 
 		// verification
-
+	}
+	
+	@Test
+	public void mustSubtractTwoNumbers() {
+		
+		// scenario
+		Calculator calculator = new Calculator();
+		int num1 = 10, num2 = 5;
+		
+		// execution
+		int result = calculator.subtraction(num1, num2);
+		
+		// verification
+		Assertions.assertThat(result).isEqualTo(5);
+	}
+	
+	public void mustMultiplyTwoNumbers() {
+		
+		// scenario
+		Calculator calculator = new Calculator();
+		int num1 = 5, num2 = 5;
+		
+		// excecution
+		int result = calculator.multiplication(num1, num2); 
+		
+		// verification
+		Assertions.assertThat(result).isEqualTo(25);
+	}
+	
+	@Test
+	public void mustDivideTwoNumbers() {
+		
+		// scenario
+		Calculator calculator = new Calculator();
+		double num1 = 10, num2 = 5;
+		
+		// execution
+		double result = calculator.division(num1, num2);
+		
+		// verification
+		Assertions.assertThat(result).isEqualTo(2);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void shouldntDivideNetagiveNumbers() {
+		
+		// scenario
+		Calculator calculator = new Calculator();
+		double num1 = 10, num2 = 0;
+		
+		// execution
+		double result = calculator.division(num1, num2);
+		
+		// verification
 	}
 
 }
@@ -46,5 +99,24 @@ class Calculator {
 			return num1 + num2;
 		}
 	}
+	
+	int subtraction(int num1, int num2) {
+		
+		return num1 - num2;
+	}
+	
+	int multiplication(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	double division(double num1, double num2) {
+		
+		if(num1 == 0 || num2 ==0) {
+			throw new RuntimeException("Division cannot be done by zero.");
+		} else {
+			return num1 / num2;
+		}
+	}
+	
 
 }
