23035ea68ba05f849a92c6d846a5c09872d0781b
Andre <andreoliveira.adv@hotmail.com>
2021-01-04 22:57:07 -0500

    Other useful Mockito resources

--- first-test/src/test/java/ca/andre/springboottest/MockitoTest.java
+++ first-test/src/test/java/ca/andre/springboottest/MockitoTest.java
@@ -5,24 +5,36 @@ import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTest {
-	
+
+	@Mock
+	List<String> list;
+
 	@Test
 	public void firstMockitoTest() {
-		
-		List<String> list = Mockito.mock(ArrayList.class);
-		
-		Mockito.when(list.size()).thenReturn(20);
-		
+
+		Mockito.mock(ArrayList.class);
+		Mockito.when(list.size()).thenReturn(2);
+
 		int size = list.size();
+		list.add("");
+
+		Assertions.assertThat(size).isEqualTo(2);
 		
-		Assertions.assertThat(size).isEqualTo(20);
+		Mockito.verify(list, Mockito.times(1)).size();
+		// Mockito.verify(list, Mockito.never()).size();
+		
+		InOrder inOrder = Mockito.inOrder(list);
+		inOrder.verify(list).size();
+		inOrder.verify(list).add("");
 		
 	}
-	
-	
-	
 
 }
