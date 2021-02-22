ff06144f899451a9ba0546ece9a8dd5335ce1113
andrerco001 <andreoliveira.adv@hotmail.com>
2021-02-22 18:24:36 -0500

    Basics for creating API's - @RestController, Http verbs and other annotations

--- first-project/src/main/java/ca/springboot/api/MyResource.java
+++ first-project/src/main/java/ca/springboot/api/MyResource.java
@@ -1,19 +1,45 @@
 package ca.springboot.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/customers")
 public class MyResource {
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Customer save(@RequestBody Customer customer) {
+		// service.save(customer);
+		return customer;
+	}
 	
-	@GetMapping("/customers/{id}")
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		// service.getById(id);
+		// service.delete(customer);
+	}
+	
+	@PutMapping("{id}")
+	public Customer update(@PathVariable Long id, @RequestBody Customer customer) {
+		// service.getById(id);
+		// service.update(customer);
+		return customer;
+	}
+
+	@GetMapping("{id}")
 	public Customer getDataCustomerById(@PathVariable Long id) {
 		System.out.println(String.format("Id recived by url: %d", id));
-		Customer customer = new Customer(1L, "Customer01", "000.000.000-00");
-		
+		Customer customer = new Customer(1L, "Customer01-new", "111.111.111-11");
 		return customer;
 	}
 
